fb2a1d2175409bd8e1c659017e1f21014e208bf3
Michael Zabala(Mike) <wonkaphreak@gmail.com>
2022-11-06 20:15:59 -0400

    add function for header search

--- src/ExcelHandle.js
+++ src/ExcelHandle.js
@@ -3,15 +3,88 @@ const ExcelJS = require("exceljs");
 class ExcelHandle {
   constructor() {}
 
-  addWorkSheet({ workbook, nameSheet }) {
+  addWorkSheet({ workbook, sheetName }) {
     if (!workbook) throw new Error("workbook not exists");
-    const sheet = workbook.addWorksheet(nameSheet);
-    return sheet;
+    const sheet = workbook.addWorksheet(sheetName);
+    return { sheet, workbook };
   }
 
   createWorkBook() {
     const workbook = new ExcelJS.Workbook();
     return workbook;
+  }
+
+  addRows(sheet, data) {
+    const cols = Object.keys(data[0]).map((prop) => {
+      return {
+        header: prop,
+        key: prop,
+        width: 45,
+      };
+    });
+    sheet.columns = cols;
+    sheet.addRows(data);
+    return true;
+  }
+
+  async writeFile(workbook, fileName) {
+    await workbook.xlsx.writeFile(fileName);
+    return true;
+  }
+
+  async handleData(sheetName, data, fileName) {
+    const wb = new ExcelJS.Workbook();
+    const sheet = wb.addWorksheet(sheetName);
+    const cols = Object.keys(data[0]).map((prop) => {
+      return {
+        header: prop,
+        key: prop,
+        width: prop.length,
+      };
+    });
+    sheet.columns = cols;
+    sheet.addRows(data);
+    await wb.xlsx.writeFile(fileName);
+    return wb;
+  }
+
+  //   getCellByName(worksheet, name) {
+  //     var match;
+  //     worksheet.eachRow(function (row) {
+  //         row.eachCell(function (cell) {
+  //             for (var i = 0; i < cell.names.length; i++) {
+  //                 if (cell.names[i] === name) {
+  //                     match = cell;
+  //                     break;
+  //                 }
+  //             }
+  //         });
+  //     });
+  //     return match;
+  // }
+
+  getCellByName(worksheet, headers) {
+    const result = [];
+    const row = worksheet.getRow(1);
+    for (let i = 1; i < row.values.length; i++) {
+      const cell = row.getCell(i);
+      result.push({ ...cell });
+    }
+    return result.filter((cell) => headers.includes(cell._column._header));
+  }
+
+  getHeaders(worksheet, index) {
+    const result = [];
+
+    const row = worksheet.getRow(index);
+
+    if (row === null || !row.values || !row.values.length) return [];
+
+    for (let i = 1; i < row.values.length; i++) {
+      const cell = row.getCell(i);
+      result.push(cell.text);
+    }
+    return result;
   }
 }
 
--- test/excel.test.spec.js
+++ test/excel.test.spec.js
@@ -2,7 +2,7 @@ const { ExcelHandle } = require("../src/ExcelHandle");
 const { Workbook } = require("exceljs");
 
 describe("Excel Handle suite test", () => {
-  it("create worbook", () => {
+  it("create workbook", () => {
     const excelHandle = new ExcelHandle();
 
     expect(excelHandle.createWorkBook()).toBeInstanceOf(Workbook);
@@ -25,13 +25,13 @@ describe("Excel Handle suite test", () => {
 
     expect(wb1).toBeInstanceOf(Workbook);
     expect(
-      excelHandle.addWorkSheet({ workbook: wb1, nameSheet: sheet1 }).id
+      excelHandle.addWorkSheet({ workbook: wb1, nameSheet: sheet1 }).sheet.id
     ).toBe(1);
     expect(
-      excelHandle.addWorkSheet({ workbook: wb1, nameSheet: sheet2 }).id
+      excelHandle.addWorkSheet({ workbook: wb1, nameSheet: sheet2 }).sheet.id
     ).toBe(2);
     expect(
-      excelHandle.addWorkSheet({ workbook: wb1, nameSheet: sheet3 }).id
+      excelHandle.addWorkSheet({ workbook: wb1, nameSheet: sheet3 }).sheet.id
     ).toBe(3);
   });
 
@@ -42,6 +42,66 @@ describe("Excel Handle suite test", () => {
     expect(wb).toBeInstanceOf(Workbook);
     expect(async () =>
       excelHandle.addWorkSheet({ nameSheet: sheet1 })
-    ).rejects.toThrowError(new Error("workbook not exists"));
+    ).rejects.toThrowError("workbook not exists");
+  });
+
+  it("Add Rows to WorkSheet", async () => {
+    const excelHandle = new ExcelHandle();
+    const wb = excelHandle.createWorkBook();
+    const { sheet } = excelHandle.addWorkSheet({
+      workbook: wb,
+      sheetName: "First Data",
+    });
+    const data = require("../Persons.json");
+    const result = await excelHandle.addRows(sheet, data);
+    expect(result).toBe(true);
+  });
+
+  it("Write File WorkBook", async () => {
+    const excelHandle = new ExcelHandle();
+    const wb = excelHandle.createWorkBook();
+    const { sheet } = excelHandle.addWorkSheet({
+      workbook: wb,
+      sheetName: "First Data",
+    });
+    const data = require("../Persons.json");
+    await excelHandle.addRows(sheet, data);
+    const flag = await excelHandle.writeFile(wb, "./test2.xlsx");
+    expect(flag).toBeTruthy();
+  });
+
+  it("Write File WorkBook from scratching", async () => {
+    const excelHandle = new ExcelHandle();
+    const sheetName = "Data";
+    const fileName = "./test.xlsx";
+    const data = require("../Persons.json");
+    const wb = await excelHandle.handleData(sheetName, data, fileName);
+    expect(wb).toBeInstanceOf(Workbook);
+  });
+
+  it("get headers from sheet", () => {
+    const excelHandle = new ExcelHandle();
+    const wb = excelHandle.createWorkBook();
+    const { sheet } = excelHandle.addWorkSheet({
+      workbook: wb,
+      sheetName: "First Data",
+    });
+    const data = require("../Persons.json");
+    excelHandle.addRows(sheet, data);
+    expect(excelHandle.getHeaders(sheet, 1)).toBeInstanceOf(Array);
+  });
+
+  it("get cell by name", () => {
+    const excelHandle = new ExcelHandle();
+    const wb = excelHandle.createWorkBook();
+    const { sheet } = excelHandle.addWorkSheet({
+      workbook: wb,
+      sheetName: "First Data",
+    });
+    const data = require("../Persons.json");
+    excelHandle.addRows(sheet, data);
+    const result = excelHandle.getCellByName(sheet, ["first_name", "email"]);
+    console.log(result);
+    expect(result).toBeTruthy();
   });
 });
